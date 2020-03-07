c00743fba4763da18ebd2b825a3fdffd196d67b9
Kevin Tran <sesouk@gmail.com>
2020-03-06 19:41:20 -0700

    removed unnecessary code

--- src/battle.js
+++ src/battle.js
@@ -12,7 +12,6 @@ const [ playerScore, setPlayerScore ] = useState(0)
 const [ rivalScore, setRivalScore ] = useState(0)
 const [ round, setRound ] = useState(0)
 const [ winner, setWinner ] = useState('')
-const [ button, setButton ] = useState('')
 const [ battleText, setBattleText ] = useState('')
 
 useEffect(() => {
@@ -21,27 +20,25 @@ useEffect(() => {
 }, [round])
 
 const restart = () => {
+  setPlayerPick('')
+  setRivalPick('')
   setRound(0)
   setWinner('')
   setBattleText('')
-  setButton('')
   setPlayerScore(0)
   setRivalScore(0)
 }
 
 const checkRound = () => {
-  if (round === 5) {
+  if (round === 5 && playerPick) {
     if (playerScore === rivalScore) {
       setWinner('You and your rival battle to a stale mate, battle again?')
-      setButton('disabled')
     }
     else if (playerScore > rivalScore) {
       setWinner('You defeated your rival in battle, battle again?')
-      setButton('disabled')
     }
     else {
       setWinner('You were defeated by your rival in battle, battle again?')
-      setButton('disabled')
     }
   }
 }
@@ -52,18 +49,23 @@ const getRivalPick = () => {
 }
 
 const fight = () => {
-  if (playerPick === rivalPick) {
-    setBattleText(`Your ${playerPick} and Rival's ${rivalPick} were evenly matched!`)
-  }
-  else if ((playerPick === 'Totodile' && rivalPick === 'Cyndaquil') ||
-          (playerPick === 'Chikorita' && rivalPick === 'Totodile')  ||
-          (playerPick === 'Cyndaquil' && rivalPick === 'Chikorita')){
-            setPlayerScore(playerScore + 1)
-            setBattleText(`Your ${playerPick} was super effective against Rival's ${rivalPick}!`)
-          }
-  else{
-    setRivalScore(rivalScore + 1)
-    setBattleText(`Rival's ${rivalPick} was super effective against your ${playerPick}!`)
+  if ( playerPick ) {
+    if (playerPick === rivalPick) {
+      setBattleText(`Your ${playerPick} and Rival's ${rivalPick} were evenly matched!`)
+    }
+    else if ((playerPick === 'Totodile' && rivalPick === 'Cyndaquil') ||
+            (playerPick === 'Chikorita' && rivalPick === 'Totodile')  ||
+            (playerPick === 'Cyndaquil' && rivalPick === 'Chikorita')){
+              setPlayerScore(playerScore + 1)
+              setBattleText(`Your ${playerPick} was super effective against Rival's ${rivalPick}!`)
+            }
+    else{
+      setRivalScore(rivalScore + 1)
+      setBattleText(`Rival's ${rivalPick} was super effective against your ${playerPick}!`)
+    }
+  } else {
+    setWinner('No Pokemon was chosen, restart the battle')
+    setRound(5)
   }
 }
 
@@ -83,7 +85,7 @@ const fight = () => {
           setPlayerPick('Chikorita');
           getRivalPick()
           }}><img src={chikoritaProf} alt='Chikorita'/></div>
-          { round < 5 ? (<button disabled={ button } onClick={() => {
+          { round < 5 ? (<button onClick={() => {
             setRound(round + 1) 
             fight()
           }}>
