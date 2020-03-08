25e6131a344da0cb71ae3579e5e6486dfb2a79a2
Kevin Tran <sesouk@gmail.com>
2020-03-07 21:09:15 -0700

    Updates

---
+++ precache-manifest.44f32c6cc12ae131d3950c1b5cabbae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8cf51f5eab100d27ffb3d712955d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b81eed2ea17daf7f4f95",
+    "url": "/static/css/main.94961556.chunk.css"
+  },
+  {
+    "revision": "afff2e94d48753021e48",
+    "url": "/static/js/2.69f711e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.69f711e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81eed2ea17daf7f4f95",
+    "url": "/static/js/main.52f6e845.chunk.js"
+  },
+  {
+    "revision": "d3fbf4bd7762745224b3",
+    "url": "/static/js/runtime-main.bb1eff07.js"
+  }
+]);
